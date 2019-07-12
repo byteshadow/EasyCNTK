3e8967b58dd89ce8f15b9f3f4ddc9c215864bfd9
Mina Eshag <mina.swe@gmail.com>
2019-07-12 23:37:48 +0100

    Added an Embedding layer

---
+++ Source/EasyCNTK/Layers/Embedding.cs
@@ -0,0 +1,32 @@
+﻿using CNTK;
+
+namespace EasyCNTK.Layers
+{
+    public sealed class Embedding : Layer
+    {
+        private readonly int _dimension;
+        public Embedding(int dimension)
+        {
+            _dimension = dimension;
+        }
+
+        public static Function Build(Variable input, int embeddingDim, DeviceDescriptor device)
+        {
+            System.Diagnostics.Debug.Assert(input.Shape.Rank == 1);
+
+            var inputDim = input.Shape[0];
+            var embeddingParameters = new Parameter(new[] { embeddingDim, inputDim }, input.DataType, CNTKLib.GlorotUniformInitializer(), device);
+            return CNTKLib.Times(embeddingParameters, input);
+        }
+
+        public override Function Create(Function input, DeviceDescriptor device)
+        {
+            return Build(input, _dimension, device);
+        }
+
+        public override string GetDescription()
+        {
+            return "Embedding";
+        }
+    }
+}
